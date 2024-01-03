b41f40c299d5a79928ffd2f11a7a4ebc6cc08ffb
amirpoint <onceler51@gmail.com>
2024-01-03 12:09:59 +0330

    initial commit

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,26 +1,30 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
+export enum ROLES {
+    USER = 'User',
+    ADMIN = 'Admin',
+}
 
 @Schema({
     timestamps: true,
 })
 export class User {
 
-    @Prop({required: true})
+    @Prop({ required: true })
     user_id: number;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     username: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     password: string;
 
-    @Prop({required: true, default: 0})
-    admin_perm: boolean;
+    @Prop({ required: true, default: 'User' })
+    admin_perm: ROLES;
 
-    @Prop({required: false})
+    @Prop({ required: false })
     avatar: string;
-    
+
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
